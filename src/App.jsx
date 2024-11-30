5a36fee762066ca8889608de6953b68cfa813bf6
Cesar Choquehuanca <cesarchoqueskater@gmail.com>
2024-11-30 04:43:13 -0500

    1er commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Navbar from './components/Navbar'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Navbar/>
+      </div>
+    </>
+  )
+}
+
+export default App
