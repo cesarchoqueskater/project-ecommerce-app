801b899712ac2dcd0954e19934ecd96ac4b747b4
Cesar Choquehuanca <cesarchoqueskater@gmail.com>
2024-12-21 00:29:10 -0500

    Fix grid-container y loading

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -6,13 +6,58 @@ function Item({ item }) {
     const [itemObject, setItemObject] = useState([])
     const navigate = useNavigate();
 
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    /*
     useEffect(() => {
         fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${item}`)
             .then(res => res.json())
             .then(res => setItemObject(res))
     }, []);
+    */
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+          try {
+            setLoading(true);
+            
+            const response = await fetch(
+              `https://collectionapi.metmuseum.org/public/collection/v1/objects/${item}`
+            );
+    
+            if (!response.ok) {
+              throw new Error("Error al obtener los registros del elemento.");
+            }
+    
+            const data = await response.json();
+            setItemObject(data);
+    
+            if (!data || data.length === 0) {
+              throw new Error("No se encontro ningun elemento.");
+            }
+    
+          } catch (err) {
+            setError(err.message); 
+          } finally {
+            setLoading(false); 
+          }
+        };
+    
+        fetchData();
+      }, []);
+    
+    // Mostrar un mensaje de carga mientras se obtienen los datos
+    if (loading) {
+        return <div className='d-flex justify-content-center p-5'>
+                    <div className="spinner-border text-primary" role="status">
+                        <span className="sr-only"></span>
+                    </div>
+                </div>;
+    }  
 
     return (
+        
         <div key={itemObject.objectID} className="card">
             <img src={itemObject.primaryImageSmall ? itemObject.primaryImageSmall : "/src/assets/img/Image-not-found.png"} className="card-img-top" alt={itemObject.title} />
             <div className="card-body">
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -5,10 +5,10 @@ import Item from './Item'
 function ItemListContainer({ items }) {
 
   return (
-    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5 g-3">
+    <div className="grid-container">
       {
         items.map((item) => (
-          <div key={item} className="col">
+          <div key={item} className="grid-item">
             <Item item={item} />
           </div>
         ))}
